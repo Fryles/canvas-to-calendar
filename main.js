7461d98b524caf0b9e56eecab01e2748cd4abe33
Kaled Barraj <kaledbarraj@Kaleds-MacBook-Air.local>
2023-05-15 17:18:53 -0700

    Calendar API works injecting events.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var courses = []; //array of courses and their respective assignments
 var token = "";
 
+// Authorization token (OAuth2.0 token)
+var authToken = "";
+
 /*
 SCRAPPED CODE FOR INJECTING SCRIPT INTO CURRENT TAB
 
@@ -26,8 +29,38 @@ window.onload = function () {
 	loadToken();
 	const scrapeBtn = document.querySelector("#scrapeBtn");
 	scrapeBtn.addEventListener("click", async () => {
-		loadAssignments();
-		console.log(courses);
+		
+		getAuthorization();
+		var aEvent = {
+			'summary': 'Google I/O 2015',
+			'location': '800 Howard St., San Francisco, CA 94103',
+			'description': 'A chance to hear more about Google\'s developer products.',
+			'start': {
+			  'dateTime': '2023-05-28T09:00:00-07:00',
+			  'timeZone': 'America/Los_Angeles'
+			},
+			'end': {
+			  'dateTime': '2023-05-28T17:00:00-07:00',
+			  'timeZone': 'America/Los_Angeles'
+			},
+			'recurrence': [
+			  'RRULE:FREQ=DAILY;COUNT=2'
+			],
+			'attendees': [
+			  {'email': 'lpage@example.com'},
+			  {'email': 'sbrin@example.com'}
+			],
+			'reminders': {
+			  'useDefault': false,
+			  'overrides': [
+				{'method': 'email', 'minutes': 24 * 60},
+				{'method': 'popup', 'minutes': 10}
+			  ]
+			}
+		  };
+		insertEvent(aEvent);
+		/*loadAssignments();
+		console.log(courses);*/
 	});
 
 	const tokenInput = document.querySelector("#floatingInput");
@@ -105,4 +138,63 @@ function loadToken() {
 			document.querySelector("#floatingInput").placeholder = token;
 		}
 	});
+}
+
+// Ask for user's permission if it can modify it's Google Calendar and gets authToken if granted.
+function getAuthorization () {
+	chrome.identity.getAuthToken({ 'interactive' : true}, function(authToken) {
+		console.log(authToken)
+	});
+}
+
+// Still working on this.
+function createEvent (/*something*/) {
+	var event = {
+		'summary': 'Google I/O 2015',
+		'location': '800 Howard St., San Francisco, CA 94103',
+		'description': 'A chance to hear more about Google\'s developer products.',
+		'start': {
+		  'dateTime': '2023-05-28T09:00:00-07:00',
+		  'timeZone': 'America/Los_Angeles'
+		},
+		'end': {
+		  'dateTime': '2023-05-28T17:00:00-07:00',
+		  'timeZone': 'America/Los_Angeles'
+		},
+		'recurrence': [
+		  'RRULE:FREQ=DAILY;COUNT=2'
+		],
+		'attendees': [
+		  {'email': 'lpage@example.com'},
+		  {'email': 'sbrin@example.com'}
+		],
+		'reminders': {
+		  'useDefault': false,
+		  'overrides': [
+			{'method': 'email', 'minutes': 24 * 60},
+			{'method': 'popup', 'minutes': 10}
+		  ]
+		}
+	  };
+}
+
+// Takes an event and inserts it into Google Calendar.
+function insertEvent(aEvent) {		
+	// Initializes the API request.
+	let init = {
+		method: 'POST',
+		async: true,
+		headers: {
+			Authorization: 'Bearer ' + authToken,
+			'Content-Type': 'application/json'
+		},		
+		body: JSON.stringify(aEvent)
+	};
+		 
+	// Fetches the API request.
+	fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', init)
+	.then((response) => response.json()) // Transform the data into json
+	.then(function(data) {
+		console.log(data);
+	  })
 }
