eb4b59711e8bf7c6b61edc700888c3b029e8dd2e
Kaled Barraj <kaledbarraj@Kaleds-MacBook-Air.local>
2023-05-19 20:28:05 -0700

    Updating for now

--- main.js
+++ main.js
@@ -1,61 +1,33 @@
+// GLOBALS
 var courses = []; //array of courses and their respective assignments
+var enrollments = []; //array of enrollments (used to get grades)
 var token = "";
-
-/*
-SCRAPPED CODE FOR INJECTING SCRIPT INTO CURRENT TAB
-
 var tabId;
-// The ID of the extension we want to talk to.
+var canvasUserObj = {}; //object containing user info
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	var tab = tabs[0];
-	console.log(tab.id);
 	tabId = tab.id;
 });
 
-	chrome.scripting
-				.executeScript({
-					target: { tabId: tabId },
-					files: ["scraping.js"],
-				})
-				.then(() => console.log("script injected"));
-*/
-
+// EVENT LISTENERS/INITIALIZATION
 window.onload = function () {
 	// This code will run when the popup DOM is ready to query
 	loadToken();
+
+	// Gets authorization from User once extension is opened.
+	getAuthorization();
+
 	const scrapeBtn = document.querySelector("#scrapeBtn");
 	scrapeBtn.addEventListener("click", async () => {
-		var event = {
-			'summary': 'Google I/O 2015',
-			'location': '800 Howard St., San Francisco, CA 94103',
-			'description': 'A chance to hear more about Google\'s developer products.',
-			'start': {
-			  'dateTime': '2023-05-28T09:00:00-07:00',
-			  'timeZone': 'America/Los_Angeles'
-			},
-			'end': {
-			  'dateTime': '2023-05-28T17:00:00-07:00',
-			  'timeZone': 'America/Los_Angeles'
-			},
-			'recurrence': [
-			  'RRULE:FREQ=DAILY;COUNT=2'
-			],
-			'attendees': [
-			  {'email': 'lpage@example.com'},
-			  {'email': 'sbrin@example.com'}
-			],
-			'reminders': {
-			  'useDefault': false,
-			  'overrides': [
-				{'method': 'email', 'minutes': 24 * 60},
-				{'method': 'popup', 'minutes': 10}
-			  ]
-			}
-		  };
-		insertEvent(event);
-		/*loadAssignments();
-		console.log(courses);*/
+		loadAssignments()
+		console.log(courses);
+	});
+
+	const selectAsgnmsBtn = document.querySelector("#asgnmBtn");
+	selectAsgnmsBtn.addEventListener("click", async () => {
+		//placeholder
+		console.log(courses);
 	});
 
 	const tokenInput = document.querySelector("#floatingInput");
@@ -65,15 +37,41 @@ window.onload = function () {
 			// if user presses enter
 			token = tokenInput.value;
 			storeToken(token);
+			getUser();
 		}
+	});
+
+	const tweakBtn = document.querySelector("#tweaksBtn");
+	tweakBtn.addEventListener("click", async () => {
+		chrome.scripting.executeScript({
+			target: { tabId: tabId },
+			files: ["./tweaks.js"],
+		});
 	});
 }; //end window.onload
 
+// FUNCTIONS
+function getUser() {
+	return fetch("https://canvas.ucsc.edu/api/v1/users/self", {
+		headers: {
+			//headers for authorization (token)
+			Accept: "application/json",
+			Authorization: "Bearer " + token,
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			canvasUserObj = data;
+			loadEnrollments();
+			return data;
+		});
+}
+
 function loadAssignments() {
+	courses = []; //reset courses
 	var base = "https://canvas.ucsc.edu/api/v1/"; //base url for canvas api
 	fetch(
-		//fetch all courses
-		base + "courses" + "?per_page=100&include[]=concluded&include[]=favorites",
+		base + "courses" + "?per_page=100&include[]=concluded&include[]=favorites", //fetch all courses
 		{
 			headers: {
 				//headers for authorization (token)
@@ -84,7 +82,7 @@ function loadAssignments() {
 	)
 		.then((response) => response.json())
 		.then((data) => {
-			if (data.errors.length > 0) {
+			if (data.errors && data.errors.length > 0) {
 				// bad request
 				alert(data.errors[0].message);
 				return;
@@ -97,9 +95,25 @@ function loadAssignments() {
 				}
 			}
 			//courses now contains all active courses
-			//now we need to get all assignments for each course
+			//now we need to get all assignments and grades for each course
+
+			let courseGrades = []; //for tweaks script
 			for (let i = 0; i < courses.length; i++) {
 				courses[i].assignments = [];
+				//get grade for each course
+				for (let j = 0; j < enrollments.length; j++) {
+					if (enrollments[j].course_id == courses[i].id) {
+						courses[i].grade = enrollments[j].grades.current_score;
+						courseGrades.push({
+							course: courses[i].name,
+							courseId: courses[i].id,
+							courseCode: courses[i].course_code,
+							courseOgName: courses[i].original_name,
+							grade: enrollments[j].grades.current_score,
+							letterGrade: enrollments[j].grades.current_grade,
+						});
+					}
+				}
 				fetch(base + "courses/" + courses[i].id + "/assignments", {
 					headers: {
 						//headers for authorization (token)
@@ -116,6 +130,29 @@ function loadAssignments() {
 						}
 					});
 			}
+			//save courseGrades to storage for tweaks script
+			chrome.storage.sync.set({ courseGrades: courseGrades }, function () {
+				console.log("saved courseGrades: ", courseGrades);
+			});
+		});
+}
+
+function loadEnrollments() {
+	return fetch(
+		"https://canvas.ucsc.edu/api/v1/users/" + canvasUserObj.id + "/enrollments",
+		{
+			headers: {
+				//headers for authorization (token)
+				Accept: "application/json",
+				Authorization: "Bearer " + token,
+			},
+		}
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			//enrollments have been fetched and converted to JSON
+			enrollments = data;
+			return enrollments;
 		});
 }
 
@@ -131,6 +168,7 @@ function loadToken() {
 		token = obj.token;
 		if (token != "" && token != null && token != undefined) {
 			document.querySelector("#floatingInput").placeholder = token;
+			getUser();
 		}
 	});
 }
@@ -168,8 +206,8 @@ function createEvent (/*something*/) {
 
 // Takes an event and inserts it into Google Calendar.
 function insertEvent(aEvent) {	
-	// Ask for user's permission if it can modify it's Google Calendar and gets authToken if granted.	
-	chrome.identity.getAuthToken({ 'interactive' : true}, function(token) {
+	// Get access token to setup initialization for API request.	
+	chrome.identity.getAuthToken({'interactive' : true}, function(token) {
 		// Initializes the API request.
 		let init = {
 			method: 'POST',
@@ -187,5 +225,12 @@ function insertEvent(aEvent) {
 		.then(function(data) {
 			console.log(data);
 	  	})
+	});
+}
+
+// Get intial authorization to access User's private Calendar data.
+function getAuthorization() {
+	chrome.identity.getAuthToken({'interactive' : true}, function(token) {
+		console.log(token)
 	});
 }
